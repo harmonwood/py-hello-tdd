c27c085895a283e5ed2967dfe9c5b2ab62ba7193
Harmon Wood <harmonwood@gmail.com>
2020-07-25 15:24:48 -0500

    fixed first test

--- tests/mytest.py
+++ tests/mytest.py
@@ -2,5 +2,5 @@ import unittest
 
 class mytests(unittest.TestCase):
 
-    def test_upper(self)
-        self.assertEqual('foo'.upper, 'FOO')
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
